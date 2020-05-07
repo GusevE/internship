f630fe51db3374d4f50b72a84dab95430a107736
bitrix <bitrix@fb29d32dcd74.(none)>
2020-05-07 22:20:49 +0300

    «Fix»

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Задание 2</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link href="style2.css" rel="stylesheet" />
+  </head>
+  <body>
+    <div class="nav">
+      <div class="nav__title">Menu</div>
+      <a class="nav__item" href="/button/check.html"> Checkbox</a>
+      <a class="nav__item" href="/glavnoe/index5.html">Verstka</a>
+      <a class="nav__item" href="/semantika/text.html"> Sematika</a>
+      <a class="nav__item" href="/One/index.html"> First task</a>
+    </div>
+  </body>
+</html>
