f0aa6555dfdd9dc4fd8cde59dd69e03ab4afba20
bitrix <bitrix@fb29d32dcd74.(none)>
2020-06-08 10:10:03 +0300

    «Fix»

--- index.php
+++ index.php
@@ -14,7 +14,9 @@
       <a href="/semantika/text.html"> <div class="nav__item"> Sematika</a></div>
       <a href="/One/index.html"> <div class="nav__item"> First task</a></div>
       <a href="/lending/index.html"> <div class="nav__item"> Lending</a></div>
-      <a href="/adaptive_grid/index.html"> <div class="nav__item"> Adaptive Grid</a></div>
+      <a href="/adaptive/index.html"> <div class="nav__item"> Adaptive</a></div>
+      <a href="/adaptive_flex/index.html"> <div class="nav__item">Adaptive_flex</a></div>
+
     </div>
   </body>
 </html>
